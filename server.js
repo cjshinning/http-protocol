15c0566a1be6ecf16a7bf519f1865bf202151404
cjshinning <784319417@qq.com>
2020-04-25 18:45:29 +0800

    3-4 缓存头Cache-control的含义和使用

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer((request, response) => {
-    console.log(`request come ${request.url}`);
-    response.writeHead(200,{
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'X-Text-Cors',
-        'Access-Control-Allow-Methods': 'POST, PUT, Delete',
-        'Access-Control-Max-Age': '1000'
-    });
-    response.end('123');
+    console.log(`request come, ${request.url}`);
+    // response.writeHead(200,{
+    //     'Access-Control-Allow-Origin': '*',
+    //     'Access-Control-Allow-Headers': 'X-Text-Cors',
+    //     'Access-Control-Allow-Methods': 'POST, PUT, Delete',
+    //     'Access-Control-Max-Age': '1000'
+    // });
+    // response.end('123');
+
+    if(request.url === '/'){
+        const html = fs.readFileSync('test.html', 'utf8');
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        response.end(html);
+    }
+
+    if(request.url === '/script.js'){
+        response.writeHead(200, {
+            'Content-Type': 'text/javascript',
+            'Cache-control': 'max-age=200'
+        })
+        response.end('console.log("script loaded loaded twice")');
+    }
 }).listen(8888);
 
 console.log('server listening on 8888');
